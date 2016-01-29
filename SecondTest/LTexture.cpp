206237746d8c0100923e85a93367903e396a3f35
ottogithub <x20121223@163.com>
2016-01-29 19:13:26 +0800

    Signed-off-by: ottogithub <x20121223@163.com>

--- SecondTest/LTexture.cpp
+++ SecondTest/LTexture.cpp
@@ -34,7 +34,7 @@ bool LTexture::loadFromFile( std::string path )
 	else
 	{
 		//Color key image
-		SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0xFF, 0xFF, 0xFF ) );
+		//SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0xFF, 0xFF, 0xFF ) );
 
 		//Create texture from surface pixels
 		newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
